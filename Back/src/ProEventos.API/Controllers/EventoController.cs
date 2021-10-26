51a74c5941b95e145abeaba63f1dd300bdeda524
Diego Francisco <deliasfrancisco@gmail.com>
2021-10-25 21:40:15 -0300

    Alterações iniciais

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento() ;
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de post";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "Exemplo de delete" + id;
+        }
+    }
+}
